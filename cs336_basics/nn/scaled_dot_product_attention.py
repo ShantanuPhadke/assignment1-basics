30990429b478ad276f8b8485eac728f577103c8b
Shantanu Phadke <shantanu.phadke@kellogg.northwestern.edu>
2025-08-19 19:04:09 -0700

    Better generalization for attention calculation across 3d and 4d tensors

--- cs336_basics/nn/scaled_dot_product_attention.py
+++ cs336_basics/nn/scaled_dot_product_attention.py
@@ -3,9 +3,9 @@ from einops import einsum
 from .softmax import softmax
 
 def scaled_dot_product_attention(queries, keys, values, mask=None):
-	pre_softmax_scores = (queries @ torch.transpose(keys, 1, 2))/torch.Tensor([queries.shape[-1]**0.5])
+	pre_softmax_scores = (queries @ torch.transpose(keys, -2, -1))/torch.Tensor([queries.shape[-1]**0.5])
 	if mask != None:
 		pre_softmax_scores = torch.where(mask == False, pre_softmax_scores+float('-inf'), pre_softmax_scores)
-	softmax_scores = softmax(pre_softmax_scores, 2)
+	softmax_scores = softmax(pre_softmax_scores, -1)
 	final_attention_outputs = torch.nan_to_num(softmax_scores) @ values
 	return final_attention_outputs
